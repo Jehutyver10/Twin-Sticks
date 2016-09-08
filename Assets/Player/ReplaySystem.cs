be552f94d726831e129acc4aa968bcc72cef9073
Jehutyver10 <juan.irizarry.cole@gmail.com>
2016-09-07 23:02:44 -0400

    Should probably commit all this, I realize

---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour {
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+	private Rigidbody rigidBody;
+	private GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		gameManager = GameObject.FindObjectOfType<GameManager>();
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameManager.recording){
+			Record();
+		} else{
+			Playback();
+		}
+	}
+	void Record(){
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+	}
+	void Playback(){
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		transform.position = keyFrames[frame].position;
+		transform.rotation = keyFrames[frame].rotation;
+	}
+}
+/// <summary>
+/// A structure for storing time, position and rotation
+/// </summary>
+public class MyKeyFrame{
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+	public MyKeyFrame(float aTime, Vector3 aPos, Quaternion aRot){
+		frameTime = aTime;
+		position = aPos;
+		rotation = aRot;
+	}
+}
---
+++ Assets/SelfieStick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfieStick : MonoBehaviour {
+	private GameObject player;
+	private Vector3 armRotation;
+
+	public float panSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		armRotation.y += Input.GetAxis("RHoriz") * panSpeed;
+		armRotation.z = Input.GetAxis("RVert") * panSpeed;
+		transform.position = player.transform.position;
+		transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
